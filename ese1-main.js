d014cfe189209d8cbff1cff1449b415498ffb020
ervinbani <bani.ervin@gmail.com>
2018-03-23 11:08:44 +0100

    ese1 jsnack bar

---
+++ ese1-main.js
@@ -0,0 +1,18 @@
+//L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+
+var num1=prompt("inserisci un numero");
+var num2=prompt("inserisci un altro numero");
+
+if(num1>num2){
+  document.write("Il numero maggiore è = ", + num1);
+
+}
+else if(num1<num2){
+  document.write("Il numero maggiore è = ", + num2);
+
+}
+
+else{
+  document.write("hai inserito due numeri uguali", + num2);
+
+}
